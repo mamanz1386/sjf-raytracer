308c4398242e4531df03a2e11fa701bc0cc9055b
Luca <me@naef.lu>
2015-09-09 15:13:39 +0200

    Added Instancing Shuttle
    Instancing Shuttle

---
+++ rt/src/rt/testscenes/InstancingShuttle.java
@@ -0,0 +1,102 @@
+package rt.testscenes;
+
+import java.io.File;
+import java.io.IOException;
+
+import rt.*;
+import rt.accelerators.BSPAccelerator;
+import rt.cameras.*;
+import rt.films.*;
+import rt.integrators.*;
+import rt.intersectables.*;
+import rt.lightsources.*;
+import rt.samplers.*;
+import rt.tonemappers.*;
+import rt.util.Timer;
+import rt.materials.*;
+
+import javax.vecmath.Matrix4f;
+import javax.vecmath.Point3f;
+import javax.vecmath.Vector3f;
+
+import org.omg.CORBA.TRANSACTION_MODE;
+
+/**
+ * Test scene for instancing and rendering the space.
+ */
+public class InstancingShuttle extends Scene {
+
+	public IntersectableList objects;
+
+	public InstancingShuttle()
+	{	
+		outputFilename = new String("../output/testscenes/InstancingShuttle");
+		
+		// Specify integrator to be used
+		integratorFactory = new PointLightIntegratorFactory();
+		
+		// Specify pixel sampler to be used
+		//samplerFactory = new OneSamplerFactory();
+		samplerFactory = new RandomSamplerFactory();
+		SPP = 1;
+		
+		// Make camera and film
+		Vector3f eye = new Vector3f(0.f,0.f,5.f);
+		Vector3f lookAt = new Vector3f(0.f,0.f,0.f);
+		Vector3f up = new Vector3f(0.f,1.f,0.f);
+		float fov = 60.f;
+		int width = 1280;
+		int height = 720;
+		float aspect = (float)width/(float)height;
+		camera = new MovableCamera(eye, lookAt, up, fov, aspect, width, height);
+		film = new BoxFilterFilm(width, height);						
+		tonemapper = new ClampTonemapper();
+		
+		// List of objects
+		objects = new IntersectableList();	
+				
+		// Planets
+			Sphere sphere = new Sphere(new Point3f(0, 0, 0), 2f);
+			sphere.material = new PerlinNoise();
+			//objects.add(sphere);
+		
+		
+		// Shuttle
+			Timer timer = new Timer();
+			Mesh mesh;
+			BSPAccelerator accelerator;
+			Matrix4f translation = new Matrix4f();
+			translation.setIdentity();
+			translation.m23 = (float) 1;
+			try
+				{
+					
+					mesh = ObjReader.read("../obj/shuttle.obj", 1.f);
+					mesh.material = new Diffuse();
+					
+					timer.reset();
+					accelerator = new BSPAccelerator(mesh);
+					System.out.printf("Accelerator computed in %d ms.\n", timer.timeElapsed());
+					
+					Instance shuttleInstance = new Instance(accelerator, translation);
+					
+					objects.add(shuttleInstance);
+					//objects.add(accelerator);
+				} 	catch(IOException e) 
+				{
+					System.out.printf("Could not read .obj file\n");
+					return;
+				}
+				
+		root = objects;
+		
+		// List of lights
+		lightList = new LightList();
+		
+		LightGeometry light = new PointLight(new Vector3f(0.f,4f,0.8f), new Spectrum(3.f, 3.f, 3.f));
+		lightList.add(light);
+		
+		light = new PointLight(new Vector3f(-0.8f,0.2f,1.f), new Spectrum(1f, 1f, 1f));
+		lightList.add(light);		
+	}
+}
