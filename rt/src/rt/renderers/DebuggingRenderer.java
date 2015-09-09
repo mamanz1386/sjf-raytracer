65dc3d48f141426d902afe5595f15ea08b33451b
panmari <smmuzi@gmail.com>
2015-09-09 16:08:44 +0200

    Added debugging renderer.

---
+++ rt/src/rt/renderers/DebuggingRenderer.java
@@ -0,0 +1,28 @@
+package rt.renderers;
+
+import java.util.concurrent.ExecutionException;
+
+import rt.Scene;
+
+public class DebuggingRenderer extends Renderer {
+
+	private final int x, y;
+
+	public DebuggingRenderer(Scene scene, int x, int y) {
+		super(scene);
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	protected void renderInternally() throws InterruptedException,
+			ExecutionException {
+		RenderTask wholeImageTask = new RenderTask(scene, x, x+1, y, y+1);
+		wholeImageTask.run();
+	}
+	
+	protected String rendererMessage() {
+		return "Only the pixel (" + x + ", " + y + ")";
+	}
+
+}
