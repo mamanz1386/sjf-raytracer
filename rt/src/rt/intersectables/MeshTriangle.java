78f3930b714fe5752039322201781cb69be33e29
Luca <me@naef.lu>
2015-09-10 11:32:58 +0200

    Now it's 11x faster
    It’s amazing ;-)

--- rt/src/rt/intersectables/MeshTriangle.java
+++ rt/src/rt/intersectables/MeshTriangle.java
@@ -31,83 +31,8 @@ public class MeshTriangle implements Intersectable {
 		this.index = index;		
 	}
 	
-	 public HitRecord intersect(Ray r)
-	{
-		float vertices[] = mesh.vertices;
-		
-		// Access the triangle vertices as follows (same for the normals):		
-		// 1. Get three vertex indices for triangle
-		int v0 = mesh.indices[index*3];
-		int v1 = mesh.indices[index*3+1];
-		int v2 = mesh.indices[index*3+2];
-		
-		// 2. Access x,y,z coordinates for each vertex
-		float x0 = vertices[v0*3];
-		float x1 = vertices[v1*3];
-		float x2 = vertices[v2*3];
-		float y0 = vertices[v0*3+1];
-		float y1 = vertices[v1*3+1];
-		float y2 = vertices[v2*3+1];
-		float z0 = vertices[v0*3+2];
-		float z1 = vertices[v1*3+2];
-		float z2 = vertices[v2*3+2];
-		
-		Point3f a = new Point3f(x0,y0,z0);
-		Point3f b = new Point3f(x1,y1,z1);
-		Point3f c = new Point3f(x2,y2,z2);
-		
-		Vector3f e = r.origin;
-		Vector3f d = r.direction;
-		
-		Matrix3f me = new Matrix3f();
-		me.m00 = a.x - b.x;
-		me.m10 = a.y - b.y;
-		me.m20 = a.z - b.z;
-		me.m01 = a.x - c.x;
-		me.m11 = a.y - c.y;
-		me.m21 = a.z - c.z;
-		me.m02 = d.x;
-		me.m12 = d.y;
-		me.m22 = d.z;
-		
-		Vector3f m = new Vector3f(a.x - e.x, a.y - e.y, a.z - e.z);
-		if (me.determinant() == 0)
-			return null;
-		
-		me.invert();
-		
-		me.transform(m);
-		
-		float t = m.z;
-		float beta = m.x;
-		float gamma = m.y;
-		float alpha = 1 -beta -gamma;
-		if(beta>0 && gamma>0 && beta+gamma<1){
-			Point3f p = r.pointAt(t);
-			
-			float normals[] = mesh.normals;
-			
-			float nx0 = normals[v0*3];
-			float nx1 = normals[v1*3];
-			float nx2 = normals[v2*3];
-			float ny0 = normals[v0*3+1];
-			float ny1 = normals[v1*3+1];
-			float ny2 = normals[v2*3+1];
-			float nz0 = normals[v0*3+2];
-			float nz1 = normals[v1*3+2];
-			float nz2 = normals[v2*3+2];
-			
-			Vector3f normal = new Vector3f(alpha*nx0 + beta*nx1 + gamma*nx2, alpha*ny0 + beta*ny1 + gamma*ny2, alpha*nz0 + beta*nz1 + gamma*nz2);
-			
-			normal.normalize();
-			return new HitRecord(t, p, normal, r.direction, this, mesh.material, 0, 0);
-			
-		}
-		
-		return null;
-	} 
 
-	/* public HitRecord intersect(Ray r)
+	public HitRecord intersect(Ray r)
 	{
 		float vertices[] = mesh.vertices;
 		
@@ -183,9 +108,7 @@ public class MeshTriangle implements Intersectable {
 		if(lambda >= epsilon){ //ray intersection
 			
 		//Position
-		Point3f position = new Point3f(0,0,0);
-		d.scale(lambda, d);
-		position.add(o, d);
+		Point3f position = r.pointAt(lambda);
 		
 		//Calculation the normal
 		float normals[] = mesh.normals;
@@ -211,7 +134,7 @@ public class MeshTriangle implements Intersectable {
 		}
 		
 	return null;
-	} */
+	}
 	
 	
 	@Override
